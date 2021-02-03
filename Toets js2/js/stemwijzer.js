4479d276998f0943d669cf8ba283a8d75c7af25a
Joey <99062903@mydavinci.nl>
2021-02-03 09:33:23 +0100

    begonnen met elementen  aanmaken.

---
+++ Toets js2/js/stemwijzer.js
@@ -0,0 +1,6 @@
+var stemwijzer = document.getElementById("stemwijzer");
+var h1 = document.createElement("h1");
+var div = document.createElement("div");
+var button1 = document.createElement("button");
+var button2 = document.createElement("button");
+var button3 = document.createElement("button");
