81e154fd9afdd91ddde000f6d0662e7ea4dc2c0f
Joey <99062903@mydavinci.nl>
2021-02-03 10:02:56 +0100

    eindelijk weergave op scherm.

--- Toets js2/js/stemwijzer.js
+++ Toets js2/js/stemwijzer.js
@@ -1,6 +1,12 @@
-var stemwijzer = document.getElementById("stemwijzer");
-var h1 = document.createElement("h1");
-var div = document.createElement("div");
-var button1 = document.createElement("button");
-var button2 = document.createElement("button");
-var button3 = document.createElement("button");
+var stemwijzer = document.getElementById("stemWijzer");
+var stelling = document.createElement("h1");
+var div = document.createElement("div")
+var button1 = document.createElement("BUTTON")
+var button2 = document.createElement("BUTTON")
+var button3 = document.createElement("BUTTON")
+
+stemwijzer.appendChild(stelling);
+stemwijzer.appendChild(div);
+div.appendChild(button1);
+div.appendChild(button2);
+div.appendChild(button3);
