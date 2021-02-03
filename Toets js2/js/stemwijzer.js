4ce6e556315964c99a23e996a80926cf64a7cb95
Joey <99062903@mydavinci.nl>
2021-02-03 10:32:50 +0100

    bezig met de functions aanmaken voor stellingen.

--- Toets js2/js/stemwijzer.js
+++ Toets js2/js/stemwijzer.js
@@ -1,12 +1,55 @@
+var arr = ["Nederland moet terug naar de Gulden.", "Er moet meer geld naar ontwikkelingshulp.", "De overheid moet afslanken", "De AOW leeftijd moet terug naar 67 jaar.", "Wiettellt moet worden gelegaliseerd.", "We moeten minder geld aan het leger uitgeven.", "Alle werkende moeten minder loonbelasting betalen.", "Er moet meer geld uitgegeven worden aan vluchtelingenopvang.", "Er moet minder geld uitgegeven worden aan de politie."];
 var stemwijzer = document.getElementById("stemWijzer");
 var stelling = document.createElement("h1");
 var div = document.createElement("div")
-var button1 = document.createElement("BUTTON")
-var button2 = document.createElement("BUTTON")
-var button3 = document.createElement("BUTTON")
+var button1 = document.createElement("BUTTON");
+var button2 = document.createElement("BUTTON");
+var button3 = document.createElement("BUTTON");
 
 stemwijzer.appendChild(stelling);
 stemwijzer.appendChild(div);
 div.appendChild(button1);
 div.appendChild(button2);
 div.appendChild(button3);
+
+stelling.innerHTML = arr[0];
+button1.innerHTML = "EENS";
+button2.innerHTML = "ONEENS";
+button3.innerHTML = "VOLGENDE";
+button3.style.display = "none";
+
+stelling.style.marginTop = "0px";
+stemwijzer.style.textAlign = "center";
+stemwijzer.style.height = "200px";
+stemwijzer.style.backgroundColor = "white";
+
+button1.style.backgroundColor = "lightgrey";
+button2.style.backgroundColor = "lightgrey";
+button3.style.backgroundColor = "lightgrey";
+
+button1.addEventListener("click", btn1Click);
+button2.addEventListener("click", btn2Click);
+button3.addEventListener("click", btn3Click);
+
+function btn1Click(){
+	button1.style.backgroundColor = "white";
+	button2.style.backgroundColor = "lightgrey";
+	button3.style.display = "inline";
+}
+
+function btn2Click(){
+	button2.style.backgroundColor = "white";
+	button1.style.backgroundColor = "lightgrey";
+	button3.style.display = "inline";
+}
+
+function btn3Click(){
+	stelling2();
+}
+
+function stelling2(){
+	button1.style.backgroundColor = "lightgrey";
+	button2.style.backgroundColor = "lightgrey";
+	button3.style.display = "none";
+	stelling.innerHTML = arr[1];
+}
